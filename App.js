405baacd75fa23a8d905e0daefd903fd1569eba7
Emilia <emiliasanguinetti.proa@gmail.com>
2026-04-23 09:23:25 -0300

    primer commit

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 import Presentacion from './components/Presentacion';
 
@@ -6,3 +7,25 @@ export default function App() {
 
   )
 }
+=======
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+>>>>>>> 9198d1c (agrego SafeAreaView y mejoras de diseño)
